33ca5dc02407697f71d607299ea074360492543a
thebat98 <thebat98@hotmail.com>
2022-05-10 09:19:36 +0200

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://thebat98.github.io/tiktok-rss-flat/"
 
 api = TikTokApi()
 
